af85e127951fddfb204c4d09cbe05c37188ae041
Sukhjinder Nahal <SN23@users.noreply.github.com>
2018-07-26 16:13:23 -0400

    created recipeAdapter

--- app/src/main/java/com/sukhjinder/baking/MainActivity.java
+++ app/src/main/java/com/sukhjinder/baking/MainActivity.java
@@ -2,12 +2,25 @@ package com.sukhjinder.baking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recipes_recycler)
+    RecyclerView recipeRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        recipeRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
     }
 }
