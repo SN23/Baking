bef3f0519c4fdd0ca7bb8c81fc927724ca9a8960
Sukhjinder <nahal.snahal@gmail.com>
2018-11-22 16:05:23 -0500

    Setup widget

---
+++ app/src/main/java/com/sukhjinder/baking/Widget/BakingWidgetProvider.java
@@ -0,0 +1,65 @@
+package com.sukhjinder.baking.Widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.sukhjinder.baking.MainActivity;
+import com.sukhjinder.baking.Model.Recipe;
+import com.sukhjinder.baking.BakingPreferences;
+import com.sukhjinder.baking.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class BakingWidgetProvider extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        Recipe recipe = BakingPreferences.loadRecipe(context);
+        if(recipe != null){
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class),0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_widget_provider);
+            views.setTextViewText(R.id.widget_recipe_name, recipe.getName());
+            views.setOnClickPendingIntent(R.id.widget_recipe_name, pendingIntent);
+
+            Intent intent = new Intent(context, BakingWidgetService.class);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            views.setRemoteAdapter(R.id.widget_recipe_listview, intent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.widget_recipe_listview);
+        }
+
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    public static void updateAppWidgets(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+}
+
---
+++ app/src/main/java/com/sukhjinder/baking/Widget/BakingWidgetService.java
@@ -0,0 +1,29 @@
+package com.sukhjinder.baking.Widget;
+
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+import com.sukhjinder.baking.Model.Recipe;
+import com.sukhjinder.baking.BakingPreferences;
+
+public class BakingWidgetService extends RemoteViewsService {
+
+    public static void updateWidget(Context context, Recipe recipe) {
+        BakingPreferences.saveRecipe(context, recipe);
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(context, BakingWidgetProvider.class));
+        BakingWidgetProvider.updateAppWidgets(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public RemoteViewsService.RemoteViewsFactory onGetViewFactory(Intent intent) {
+        intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+
+        return new ListRemoteViewsFactory(getApplicationContext());
+    }
+
+}
